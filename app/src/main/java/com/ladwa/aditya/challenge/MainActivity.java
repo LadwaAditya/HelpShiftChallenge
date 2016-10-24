60ef160fadd43fc566a2070dd58b1df615091dfa
Aditya <ladwa.aditya@gmail.com>
2016-10-24 14:00:02 +0530

    Fect: Added Click listener to Download ImageView

--- app/src/main/java/com/ladwa/aditya/challenge/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/challenge/MainActivity.java
@@ -1,13 +1,25 @@
 package com.ladwa.aditya.challenge;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.imageview_download)
+    public void onClickImageView() {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
     }
 }
